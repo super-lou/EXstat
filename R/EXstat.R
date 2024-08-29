868b2d811d5a6f488a78cbc8a23402d43db0c451
louis.heraut <louis.heraut@inrae.fr>
2024-08-29 14:40:11 +0200

    add global doc

--- R/EXstat.R
+++ R/EXstat.R
@@ -2,9 +2,9 @@
 #' @title EXstat
 #' @description Provides an efficient and simple solution to aggregate and analyze the stationarity of time series.
 #' 
-#' @details See [EXstat github documentation](https://github.com/super-lou/EXstat) for an easier approach.
+#' @details See [EXstat github documentation](https://github.com/super-lou/EXstat) <https://github.com/super-lou/EXstat> for an easier approach.
 #'
-#' For a more user-friendly interaction, this package has been developed in symbiosis with predefined parameter files called [CARD](https://github.com/super-lou/CARD/).
+#' For a more user-friendly interaction, this package has been developed in symbiosis with predefined parameter files called [CARD](https://github.com/super-lou/CARD/) <https://github.com/super-lou/CARD/>.
 #' So you don't have to define complex parameters yourself to extract hydroclimatological variables. What's more, if the CARD you want doesn't exist, it's easy to create one based on the others.
 #'
 #' @author
@@ -21,8 +21,8 @@
 #' * Nathan Pellerin
 #' 
 #' @seealso
-#' * [process_extraction()] for extracting variables.
-#' * [process_trend()] for performing trend analysis on extracted variables.
-#' * [CARD_extraction()] for extracting variables using CARD parameterization files.
-#' * [CARD_management()] for managing CARD parameterization files.
+#' - [process_extraction()] for extracting variables.
+#' - [process_trend()] for performing trend analysis on extracted variables.
+#' - [CARD_extraction()] for extracting variables using CARD parameterization files.
+#' - [CARD_management()] for managing CARD parameterization files.
 NULL
